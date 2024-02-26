20c5b6c90582b3e8a49e28c57de45eb3d2c01e1b
LGunay <gqasimova93@gmail.com>
2024-02-26 17:15:15 +0000

    fixed if condition

--- models/base_model.py
+++ models/base_model.py
@@ -6,9 +6,6 @@ import uuid
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
         tformat = "%Y-%m-%dT%H:%M:%S.%f"
         if len(kwargs) != 0:
             for k, v in kwargs.items():
@@ -21,6 +18,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
