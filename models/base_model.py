c418abeb4efed7cb26e98183d48d80d5bce5ff2e
seyyar95 <heydrovsyyar@gmail.com>
2024-02-27 10:53:56 +0000

    File Storage updated

--- models/base_model.py
+++ models/base_model.py
@@ -2,16 +2,12 @@
 
 from datetime import datetime
 import uuid
-from __init__ import storage
+import models
 
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
         tformat = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
         if len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
@@ -21,14 +17,17 @@ class BaseModel:
                 else:
                     self.__dict__[k] = v
         else:
-            storage.new(self)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         empty_d = self.__dict__.copy()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
 #!usr/bin/python3
 import json
-
+from models.base_model import BaseModel
 
 class FileStorage:
     __file_path = "file.json"
@@ -11,18 +11,25 @@ class FileStorage:
 
     def new(self, obj):
        name = obj.__class__.__name__
-       self.__objects["name"+obj.id] = obj
+       self.__objects[f"{name}.{obj.id}"] = obj
     
     def save(self):
         file = self.__file_path
         odict = self.__objects
+        newD = {}
+        for k, v in odict.items():
+            newD[k] = v.to_dict()
+
         with open(file, "w") as f:
-            json.dump(odict, f)
+            json.dump(newD, f)
 
     def reload(self):
         file = self.__file_path
+        
         try:
             with open(file, "r") as f:
-                return json.load(f)
-        except Exception:
+                data = json.load(f)
+            for k in data:
+                self.__objects[k] = BaseModel(**data[k])
+        except FileNotFoundError:
             pass
