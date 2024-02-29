765ac710d511caf84f4d557f8e69218844eb8947
seyyar95 <heydrovsyyar@gmail.com>
2024-02-29 08:06:51 +0000

    Console is great now

--- console.py
+++ console.py
@@ -173,7 +173,7 @@ class HBNBCommand(cmd.Cmd):
             elif len(args) < 2:
                 print("** instance id missing **")
             elif len(args) < 3:
-                print("** attributae name missing **")
+                print("** attribute name missing **")
             elif len(args) < 4:
                 print("** value missing **")
 
