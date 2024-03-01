b209507d6c9cc99cd54aa11596428d0ee85ca7c2
seyyar95 <heydrovsyyar@gmail.com>
2024-03-01 15:17:32 +0000

    Place added

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -8,7 +8,7 @@ from models.place import Place
 class TestPlace(unittest.TestCase):
 
     def test_place(self):
-        instance = State()
+        instance = Place()
         instance.name = "Chinar Plaza"
         instance.city_id = 16
         instance.user_id = 23232
@@ -20,4 +20,14 @@ class TestPlace(unittest.TestCase):
         instance.latitude = 0.0
         instance.longitude = 0.0
         instance.amenity_ids = []
-        self.assertIn(instance.name, instance.__dict__)
+        self.assertEqual(instance.name, "Chinar Plaza")
+        self.assertEqual(instance.city_id, 16)
+        self.assertEqual(instance.user_id, 23232)
+        self.assertEqual(instance.description, "Something meaningful")
+        self.assertEqual(instance.number_rooms, 0)
+        self.assertEqual(instance.number_bathrooms, 0)
+        self.assertEqual(instance.max_guest, 0)
+        self.assertEqual(instance.price_by_night, 0)
+        self.assertEqual(instance.latitude, 0.0)
+        self.assertEqual(instance.longitude, 0.0)
+        self.assertEqual(instance.amenity_ids, [])
