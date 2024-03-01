2a1a02b2eac61bc5791df88d3ac3515b00b48df3
LGunay <gqasimova93@gmail.com>
2024-03-01 13:24:27 +0000

    added save() in file.storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,11 +3,17 @@
 
 import unittest
 from engine.file_storage.py import FileStorage
+from models.base_model import BaseModel
+from models import storage
 
 
 class TestFileStorage(unittest.TestCase):
 
     def test_file_path(self):
-        
         self.assertIn(__file_path, FileStorage)
+
+    def test_save(self):
+        instance = BaseModel()
+        storage.new(instance)
+        self.assertIn(instance, storage.all())
 
