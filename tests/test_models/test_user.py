dc365ebb5e84f6aa53d00a8376fdd0a6d4656b31
LGunay <gqasimova93@gmail.com>
2024-03-01 13:43:42 +0000

    fixed test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -11,11 +11,11 @@ class TestUser(unittest.TestCase):
 
     def test_user(self):
         instance = User()
-        instance.email = ""
+        instance.email = "sayyarheydarov"
         instance.password = ""
         instance.first_name = ""
         instance.last_name = ""
-        self.assertIn("email", instance.__dict__)
-        self.assertIn("password", instance.__dict__)
+        self.assertIn(instance.email, instance.__dict__)
+        self.assertIn(instance.password, instance.__dict__)
         self.assertIn("first_name", instance.__dict__)
         self.assertIn("last_name", instance.__dict__)
